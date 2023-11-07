753fdb40266d4e1ce16fc40150f55fe4cd105d86
Jojoaky <126272863+Jojoaky@users.noreply.github.com>
2023-11-07 19:42:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ document.addEventListener('scroll', (e) => {
         const letterY = letterRect.top + letterRect.height / 2;
         const windowY = Math.max((window.innerHeight / 2), letterY);
         const deltaY = Math.min((windowY - letterY) * 0.01, 8);
-        letter.style.fontSize = `${Math.max(8-deltaY, 4)}vw`
+        letter.style.fontSize = `${Math.min(Math.max(8-deltaY, 4), 7)}vw`
         if(index == 0)
         console.log(letter.style.fontSize);
     });
